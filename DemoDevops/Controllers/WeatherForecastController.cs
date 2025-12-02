fb496aa6af16c8235554164a5b70b247a82146a2
Thanh98-bg <57055762+Thanh98-bg@users.noreply.github.com>
2025-12-02 22:44:33 +0700

    Fix formatting in WeatherForecastController

--- DemoDevops/Controllers/WeatherForecastController.cs
+++ DemoDevops/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DemoDevops.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Bracing", "Cloudy"
+            "Freezing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Bracing"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
